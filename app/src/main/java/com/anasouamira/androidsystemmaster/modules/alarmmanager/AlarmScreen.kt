7b80492ab5a07900dd05b708817a91a58dfe24da
Ouamira <anasouamira@gmail.com>
2026-02-05 11:42:44 +0100

    Created By Anas Ouamira

--- app/src/main/java/com/anasouamira/androidsystemmaster/modules/alarmmanager/AlarmScreen.kt
+++ app/src/main/java/com/anasouamira/androidsystemmaster/modules/alarmmanager/AlarmScreen.kt
@@ -4,6 +4,7 @@ import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
@@ -11,23 +12,45 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import android.provider.Settings
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun AlarmScreen() {
     val context = LocalContext.current
+
     Column(Modifier.padding(16.dp)) {
         Button(onClick = {
+            val alarmManager =
+                context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                if (!alarmManager.canScheduleExactAlarms()) {
+                    val intent = Intent(
+                        Settings.ACTION_REQUEST_SCHEDULE_EXACT_ALARM
+                    )
+                    context.startActivity(intent)
+                    return@Button
+                }
+            }
+
             val intent = Intent(context, AlarmReceiver::class.java)
             val pendingIntent = PendingIntent.getBroadcast(
-                context, 0, intent,
+                context,
+                0,
+                intent,
                 PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
             )
-            val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+
             val triggerTime = System.currentTimeMillis() + 5000
-            alarmManager.setExact(AlarmManager.RTC_WAKEUP, triggerTime, pendingIntent)
+            alarmManager.setExact(
+                AlarmManager.RTC_WAKEUP,
+                triggerTime,
+                pendingIntent
+            )
         }) {
             Text("Set Alarm (5s)")
         }
     }
 }
+
