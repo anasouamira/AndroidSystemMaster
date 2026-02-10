dc2c7167c2d148d3278536fda07c564236b9391e
Ouamira <anasouamira@gmail.com>
2026-02-10 12:00:48 +0100

    Created By Anas Ouamira

--- app/src/main/java/com/anasouamira/androidsystemmaster/modules/workmanager/WorkScreen.kt
+++ app/src/main/java/com/anasouamira/androidsystemmaster/modules/workmanager/WorkScreen.kt
@@ -1,5 +1,6 @@
 package com.anasouamira.androidsystemmaster.modules.workmanager
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
@@ -9,17 +10,40 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
+import java.util.concurrent.TimeUnit
 
 @Composable
 fun WorkScreen() {
     val context = LocalContext.current
-    Column(Modifier.padding(16.dp)) {
+
+    Column(
+        modifier = Modifier.padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        Text("Module 2: WorkManager")
+
         Button(onClick = {
-            val workRequest = OneTimeWorkRequestBuilder<DemoWorker>().build()
+            // مهمة مرة واحدة
+            val workRequest =
+                OneTimeWorkRequestBuilder<SyncPostsWorker>().build()
+
             WorkManager.getInstance(context).enqueue(workRequest)
         }) {
-            Text("Start Background Work")
+            Text("Sync Posts Once")
+        }
+
+        Button(onClick = {
+            // مهمة متكررة كل 15 دقيقة
+            val periodicWork =
+                PeriodicWorkRequestBuilder<SyncPostsWorker>(
+                    15, TimeUnit.MINUTES
+                ).build()
+
+            WorkManager.getInstance(context).enqueue(periodicWork)
+        }) {
+            Text("Auto Sync Every 15 min")
         }
     }
 }
